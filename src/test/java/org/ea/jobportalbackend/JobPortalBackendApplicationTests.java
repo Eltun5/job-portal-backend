320aef74b44a8b7aec0542fea388718374a4ed07
Eltun <aslanoveltun28@gmail.com>
2025-04-09 11:33:47 +0400

    init commit

---
+++ src/test/java/org/ea/jobportalbackend/JobPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ea.jobportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
